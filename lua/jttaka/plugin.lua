e9214edd4d5265880aa024b17344cb02ef5bff83
jttaka <john.t.takala@gmail.com>
2026-01-05 15:14:49 +0200

    add simple terminal

--- lua/jttaka/plugin.lua
+++ lua/jttaka/plugin.lua
@@ -3,4 +3,5 @@ vim.pack.add({"git@github.com:jbyuki/instant.nvim"})
 vim.pack.add({"git@github.com:rose-pine/neovim"})
 vim.pack.add({"git@github.com:stevearc/oil.nvim"})
 vim.pack.add({"git@github.com:tpope/vim-fugitive"})
+vim.pack.add({"git@github.com:jttaka/simple-term"})
 
